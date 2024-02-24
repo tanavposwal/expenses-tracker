3a9098b28826be5bedf88577d42b7d3abd87d58c
tanavposwal <tanavposwal939@gmail.com>
2024-02-24 16:18:35 +0530

    cors

--- backend/main.js
+++ backend/main.js
@@ -11,7 +11,9 @@ const SECRET = process.env.SECRET;
 export const app = express();
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
-app.use(cors());
+app.use(cors({ 
+	origin : ['https://expenses-trackr.vercel.app/'], 
+ } ));
 
 import jwt from "jsonwebtoken";
 import { authenticateJwt, verifyToken } from "./middleware/token.js";
