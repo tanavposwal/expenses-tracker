802096813c00426708e18164d772da201df1da84
tanavposwal <tanavposwal939@gmail.com>
2024-02-24 17:04:55 +0530

    fix: deployment cors

--- backend/main.js
+++ backend/main.js
@@ -15,7 +15,7 @@ app.use(cors({
 	origin: ['https://expenses-trackr.vercel.app/'],
 }));
 app.use((req, res, next) => {
-	res.header('Access-Control-Allow-Origin', 'http://localhost:5173');
+	res.header('Access-Control-Allow-Origin', 'https://expenses-trackr.vercel.app/');
 	res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE');
 	res.header('Access-Control-Allow-Headers', 'Content-Type,Authorization');
 	next();
